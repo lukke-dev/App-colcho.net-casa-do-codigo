18107de475921f6932b502f7443f73eb70f38c15
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-09-08 19:38:57 -0300

    config pt-Br.yml model/user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @rooms = Room.limit(3)
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,8 @@
 class Room < ApplicationRecord
+  validates :title, :location, :description, presence: true
+  validates :description, length: { minimum: 30 }
+
+  def complete_name
+    "#{title}, #{location}"
+  end
 end
