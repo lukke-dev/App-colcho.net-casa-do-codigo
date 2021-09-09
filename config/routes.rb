1504bef3d6469ac08e9ac90b461ddcbeb0b5e623
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-09-09 01:00:45 -0300

    truncate description

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  LOCALES = /en|pt-BR/
-
-  scope '(:locale)', locale: LOCALES do
+  scope '(:locale)', locale: /en|pt-BR/ do
     resources :rooms
     resources :users
   end
-  get '/:locale' => 'home#index', :locale => LOCALES
+  get '/:locale' => 'home#index', :locale => /en|pt-BR/
   root 'home#index'
 end
