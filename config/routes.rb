b3b080eb97cd9726c2b75acf7a902ca39a1d647b
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-09-09 00:47:09 -0300

    fix routes.rb to deploy heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   LOCALES = /en|pt-BR/
-  root 'home#index'
 
   scope '(:locale)', locale: LOCALES do
     resources :rooms
     resources :users
   end
-  match '/:locale': 'home#index', locale: LOCALES
+  get '/:locale' => 'home#index', :locale => LOCALES
+  root 'home#index'
 end
